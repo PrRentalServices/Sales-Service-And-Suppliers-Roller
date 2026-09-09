425ff95ffb27cbaa81fc44252923db9190ab3253
PR RENTAL SERVICES <vikascscjansewakendra@gmail.com>
2026-09-09 12:45:47 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const PR_CONFIG = {
+  businessName: "PR RENTAL SERVICES",
+  email: "sales.prrentals25@gmail.com",
+  phones: ["7892123389", "9980615715"],
+  whatsapp: "917892123389",
+  gstin: "29BFVPP3412E1Z6",
+  address: "Ground Floor, No. 194, 6th Cross, Maruthi Nagar, Near G R Kalyana Mandapa, Nagashetty Halli, RMV Extension 2nd Stage, Bengaluru Urban, Karnataka – 560094",
+  UPI_ID: "9591887914-2@axl",
+  paymentQrFile: "assets/payment-qr.png",
+  dynapacOfficial: "https://dynapac.com/in-en/"
+};
